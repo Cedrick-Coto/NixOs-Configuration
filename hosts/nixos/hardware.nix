c1296533dea7df790689a98a590192f35aef5cc5
Cedrick-Coto <cedrick.coto@gmail.com>
2026-06-21 12:28:18 -0600

    Fix hardware.nix to use relative path for flake install compatibility

--- hosts/nixos/hardware.nix
+++ hosts/nixos/hardware.nix
@@ -1,11 +1,3 @@
-# Hardware especifico de esta maquina.
-# Generado por: nixos-generate-config
-# Ruta real en el sistema: /etc/nixos/hardware-configuration.nix
-#
-# Para usar este archivo en lugar del generado:
-#   cp /etc/nixos/hardware-configuration.nix hosts/nixos/hardware.nix
-#   y descomentar la linea en default.nix
-
 { ... }: {
-  imports = [ /etc/nixos/hardware-configuration.nix ];
+  imports = [ ./hardware-configuration.nix ];
 }
